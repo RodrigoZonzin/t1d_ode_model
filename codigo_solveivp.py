b094952a5aa805fb1e3b4db47656b3d3eba82c38
rodrigozonzin <rodrigojzonzin@lab-02-07-14.dcomp>
2025-04-08 10:32:43 -0300

    solvevp att

--- codigo_solveivp.py
+++ codigo_solveivp.py
@@ -1,4 +1,4 @@
-import argparse, contextlib, sys, os
+import argparse, contextlib, sys, ois
 import scipy
 import numpy as np
 
@@ -18,30 +18,173 @@ def constants() -> list:
 
 def constants_with_names() -> list:
     constants_list = [        
-        ("r", 0.1),
-        ]
+        ('J', 50000.0), 
+        ('k', 0.4), 
+        ('b', 0.09), 
+        ('c', 0.1), 
+        ('fM', 6.23e-07), 
+        ('e1', 1e-08), 
+        ('e2', 1e-08), 
+        ('alphaB', 0.0334),
+        ('sigmaB', 0.016666666666666666), 
+        ('etha', 0.02), 
+        ('Ghb', 90), 
+        ('sE', 1), 
+        ('sR', 36), 
+        ('Bconv', 259000.0), 
+        ('Qpanc', 0.194), 
+        ('d', 0.5), 
+        ('fMa', 6.23e-07), 
+        ('ftD', 1.1899e-06), 
+        ('Dss', 100000.0), 
+        ('fD', 1.7101e-07), 
+        ('R0', 864), 
+        ('G0', 1.44), 
+        ('SI', 0.72), 
+        ('sigmaI', 43.2), 
+        ('GI', 141.4214), 
+        ('deltaI', 432), 
+        ('bDE', 4.87e-06), 
+        ('muD', 0.51), 
+        ('bIR', 4.87e-06), 
+        ('aE', 0.1199), 
+        ('Tnaive', 370), 
+        ('Qspleen', 0.1), 
+        ('bp', 12), 
+        ('thetaD', 212000.0), 
+        ('ram', 0.01), 
+        ('bE', 0.001), 
+        ('muE', 2e-06), 
+        ('aR', 0.1199), 
+        ('bR', 0.001), 
+        ('muR', 2e-06), 
+        ('aEm', 0.01)]              ]
     return constants_list
 
 
 def variable_names() -> list[str]:
     return [
-        "N",
+        "M",
+        "Ma",
+        "B",
+        "Ba",
+        "Bn",
+        "G",
+        "I",
+        "D", 
+        "tD",
+        "E",
+        "R",
+        "Em"
         ]
 
-def system(t: np.float64, y: np.ndarray, *constants) -> np.ndarray:
+def system(t: np.float64, u: np.ndarray, *constants) -> np.ndarray:
     # populations
-    N = y
-    # constants
-    r = constants
-    
-    dN_dt = r*N
+    """Variaveis do modelo"""
+    M   = u[0]
+    Ma  = u[1]
+    B   = u[2]
+    Ba  = u[3]
+    Bn  = u[4]
+    G   = u[5]
+    I   = u[6]
+    D   = u[7]
+    tD  = u[8]
+    E   = u[9]
+    R   = u[10]
+    Em  = u[11]
 
-    return np.array([dN_dt])
+    #constants
+    r = constants
+    J       = paper_parameters['J']
+    k       = paper_parameters['k']
+    b       = paper_parameters['b']
+    c       = paper_parameters['c']
+    fM      = paper_parameters['fM']
+    e1      = paper_parameters['e1']
+    e2      = paper_parameters['e2']
+    alphaB  = paper_parameters['alphaB']
+    sigmaB  = paper_parameters['sigmaB']
+    etha    = paper_parameters['etha']
+    Ghb     = paper_parameters['Ghb']
+    sE      = paper_parameters['sE']
+    sr      = paper_parameters['sR']
+    Bconv   = paper_parameters['Bconv']
+    Qpanc   = paper_parameters['Qpanc']
+    d       = paper_parameters['d']
+    fMa     = paper_parameters['fMa']
+    ftD     = paper_parameters['ftD']
+    Dss     = paper_parameters['Dss']
+    fD      = paper_parameters['fD']
+    R0      = paper_parameters['R0']
+    G0      = paper_parameters['G0']
+    SI      = paper_parameters['SI']
+    sigmaI  = paper_parameters['sigmaI']
+    GI      = paper_parameters['GI']
+    deltaI  = paper_parameters['deltaI']
+    bDE     = paper_parameters['bDE']
+    muD     = paper_parameters['muD']
+    bIR     = paper_parameters['bIR']
+    aE      = paper_parameters['aE']
+    Tnaive  = paper_parameters['Tnaive']
+    Qspleen = paper_parameters['Qspleen']
+    bp      = paper_parameters['bp']
+    thetaD  = paper_parameters['thetaD']
+    ram     = paper_parameters['ram']
+    bE      = paper_parameters['bE']
+    muE     = paper_parameters['muE']
+    aR      = paper_parameters['aR']
+    bR      = paper_parameters['bR']
+    muR     = paper_parameters['muR']
+    aEm     = paper_parameters['aEm']    
+
+
+
+    """Equacoes do modelo"""
+    #Macrophage population (1)
+    dMdt = J + (k+b)*Ma -c*M -fM*M*Ba -fM*M*Bn -e1*M*(M+Ma)
+
+    #Activated Macrophage pop (2)
+    dMadt = fM*M*Ba + fM*M*Bn -e2*Ma*(M+Ma)
+
+    #Healthy beta cells (3)
+    dBdt = alphaB*K1(G)*B -sigmaB*B -etha*K2(E,R)*B -W(B,t)
+
+    #Apoptotic beta cells (4)
+    conv_cte = Bconv/Qpanc
+    dBadt = (sigmaB*conv_cte)*B + (etha*conv_cte)*K2(E,R)*B +(W(B,t)*conv_cte) -d*Ba -fM*M*Ba - fMa*Ma*Ba -ftD*(Dss - D)*Ba -fD*D*Ba
+
+    #Necrotic beta cells (5)
+    dBndt = d*Ba -fM*M*Bn -fMa*Ma*Bn -ftD*(Dss - D)*Bn -fD*D*Bn
+
+     #Glucose (6)
+    dGdt = R0 - (G0+SI*I)*G
+
+    #Insulin (7)
+    dIdt = deltaI * (G**2/(G**2 + G*I**2))*B -sigmaI*I
+
+    #Immunogenic Dendritic Cells (8)
+    dDdt = ftD*Bn*(Dss -D -tD) +ftD*Bn*t*D -bDE*E*D -muD*D
+
+    #Tolerogenic Dendritic Cells (9)
+    dtDdt = ftD*Ba*(Dss-D-t*D) -ftD*Bn*t*D -bIR*R*t*D -muD*t*D
+
+    #Effector T-cells (10)
+    dEdt = aE*(Tnaive/Qspleen - E) +bp*(D*E/(thetaD+D)) -ram*E +bE*D*Em -muE*E*R
+
+    #Regulatory T-cells (11)
+    dRdt = aR*(Tnaive/Qspleen - R) +bp*(t*D*R/(thetaD+t*D)) -ram*R +bR*t*D*Em - muR*E*R
+
+    #Memory T-cells (12)
+    dEmdt = ram*(E+R) -(aEm +bE*D +bR*t*D)*Em
+
+    return np.array([dMdt, dMadt, dBdt, dBadt, dBndt, dGdt, dIdt, dDdt, dtDdt, dEdt, dRdt, dEmdt])
+
 
 # includes! "ode-support.py"
 
 
-def simulation_output_to_csv(sim_steps, simulation_output, write_to):
+def simulatiion_output_to_csv(sim_steps, simulation_output, write_to):
     if not simulation_output.success:
         print(simulation_output.message)
         return
---
+++ params_tuples.py
@@ -0,0 +1,7 @@
+from parameters_pops import *
+
+
+print([(i, j) for i, j in paper_parameters.items()], end = '\n')
+
+
+print([i for i, j in paper_parameters.items()])
